215ebbd59b7cc5c8d027f024b5db0b223a1ba992
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-01-29 11:19:52 +0100

    Initial commit

---
+++ src/mysql/Database.java
@@ -0,0 +1,37 @@
+package mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Database {
+    public void showCities(String country){
+        //prepare statement
+        String query = "SELECT city.Name, CountryCode " +
+                "FROM city " +
+                "INNER JOIN country ON country.Code = city.CountryCode " +
+                "WHERE country.Name LIKE ?";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //connect
+            Connection connection = DriverManager.getConnection(DatabaseID.getUrl(), DatabaseID.getUsername(), DatabaseID.getPassword());
+            if (connection != null) {
+                System.out.println("Success");
+                //make statement
+                PreparedStatement ps = connection.prepareStatement(query);
+                //insert, update, delete executeUpdate
+                //store to ResultSet
+                ps.setString(1, country);
+                System.out.println(ps);
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()){
+                    String city = rs.getString("Name");
+                    String code = rs.getString("CountryCode");
+                    System.out.println(city + " " + code);
+                }
+                connection.close();
+            }
+        }catch (Exception e) { e.printStackTrace(); }
+    }
+}
---
+++ src/mysql/Main.java
@@ -0,0 +1,8 @@
+package mysql;
+
+public class Main {
+    public static void main(String[] args) {
+        Database dat = new Database();
+        dat.showCities("Greece");
+    }
+}
