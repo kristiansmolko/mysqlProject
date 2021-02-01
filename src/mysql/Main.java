5cab62d73d166a584a3de9f8105d4eeb4ec6c2cd
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-02-01 11:06:25 +0100

    Modified main

--- src/mysql/Main.java
+++ src/mysql/Main.java
@@ -1,8 +1,15 @@
 package mysql;
 
+import mysql.entity.City;
+import mysql.output.Output;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Database dat = new Database();
-        dat.showCities("Greece");
+        List<City> cities = dat.getCities("Italy");
+        Output out = new Output();
+        out.printCities(cities);
     }
 }
