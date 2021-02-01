6904683072594997d7b2e59290b3def4aa3da318
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-02-01 13:52:13 +0100

    Added language list to country class

--- src/mysql/entity/Country.java
+++ src/mysql/entity/Country.java
@@ -1,18 +1,23 @@
 package mysql.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Country {
     private String name;
     private String code3;
     private String capitalCity;
     private int area;
     private String continent;
+    private List<String> languages;
 
-    public Country(String name, String code3, String capitalCity, int area, String continent) {
+    public Country(String name, String code3, String capitalCity, int area, String continent, List languages) {
         this.name = name;
         this.code3 = code3;
         this.capitalCity = capitalCity;
         this.area = area;
         this.continent = continent;
+        this.languages = languages;
     }
 
     public String getName() {
@@ -33,5 +38,9 @@ public class Country {
 
     public String getContinent() {
         return continent;
+    }
+
+    public List<String> getLanguages() {
+        return languages;
     }
 }
