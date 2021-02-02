2d4ce61cb93a1686a9c7fb095e17e8909bb60444
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-02-02 10:16:38 +0100

    Added additional variables to city

--- src/mysql/entity/City.java
+++ src/mysql/entity/City.java
@@ -3,6 +3,16 @@ package mysql.entity;
 public class City {
     private String name;
     private int population;
+    private String district;
+    private String code3;
+    private String country;
+
+    public City(String name, int population, String district, String country) {
+        this.name = name;
+        this.population = population;
+        this.district = district;
+        this.country = country;
+    }
 
     public City(String name, int population) {
         this.name = name;
@@ -15,5 +25,21 @@ public class City {
 
     public int getPopulation() {
         return population;
+    }
+
+    public void setCode3(String code3) {
+        this.code3 = code3;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public String getCode3() {
+        return code3;
+    }
+
+    public String getCountry() {
+        return country;
     }
 }
