ff879a7bb102c27dd51d1f34425abb08bfd25d0d
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-02-03 09:29:02 +0100

    Updated main with update call

--- src/mysql/Main.java
+++ src/mysql/Main.java
@@ -10,12 +10,13 @@ public class Main {
     public static void main(String[] args) {
         Database dat = new Database();
         Output out = new Output();
-        String countryName = "Afghanistan";
-        Country country = dat.getCountryInfo(countryName);
+        String countryName = "Slovakia";
+        /*Country country = dat.getCountryInfo(countryName);
         out.printCountryInfo(country);
         List<City> cities = dat.getCities(countryName);
-        out.printCities(cities);
-        City newCity = new City("Ploske", 68, "Revuca Okres", "Slovakia");
-        //dat.insertCity(newCity);
+        out.printCities(cities);*/
+        /*City newCity = new City("Ploske", 68, "Revuca Okres", "Slovakia");
+        dat.insertCity(newCity);*/
+        dat.updatePopulation("Slovakia", "Ploske", 69);
     }
 }
