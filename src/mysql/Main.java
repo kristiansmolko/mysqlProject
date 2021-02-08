f9944455cbdb4ca22c79420bbb07eddb5c9b5e78
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-02-08 11:23:18 +0100

    Modified main

--- src/mysql/Main.java
+++ src/mysql/Main.java
@@ -3,6 +3,8 @@ package mysql;
 import mysql.entity.CapitalCity;
 import mysql.entity.City;
 import mysql.entity.Country;
+import mysql.entity.Monument;
+import mysql.helper.Help;
 import mysql.output.Output;
 
 import java.util.List;
@@ -16,10 +18,16 @@ public class Main {
         out.printCountryInfo(country);
         List<City> cities = dat.getCities(countryName);
         out.printCities(cities);*/
+
         /*City newCity = new City("Ploske", 68, "Revuca Okres", "Slovakia");
         dat.insertCity(newCity);*/
         //dat.updatePopulation("Slovakia", "Ploske", 68);
-        List<CapitalCity> capitalCityList = dat.getCapitalCities("Europe");
-        dat.printCapitalCities(capitalCityList);
+
+        /*List<CapitalCity> capitalCityList = dat.getCapitalCities("Asia");
+        out.printCapitalCities(capitalCityList);*/
+
+        //dat.insertNewMonument("svk", "Kosice", "Dom sv. Alzbety");
+        List<Monument> monuments = Help.getMonuments();
+        out.printMonuments(monuments);
     }
 }
