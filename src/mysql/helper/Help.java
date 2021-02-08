6b65ae23910dd6f260cc966034c8c11a2e42611a
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-02-08 11:02:43 +0100

    Added help class for additional methods

---
+++ src/mysql/helper/Help.java
@@ -0,0 +1,169 @@
+package mysql.helper;
+
+import mysql.DatabaseID;
+import mysql.entity.City;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class Help {
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(DatabaseID.getUrl(), DatabaseID.getUsername(), DatabaseID.getPassword());
+    }
+
+    public static String getCountryCode(String country){
+        if (country == null || country.equalsIgnoreCase("")) {
+            System.out.println("\033[31mWrong country name!\033[0m");
+            return null;
+        }
+        String query = "SELECT Code FROM country WHERE Name LIKE ?";
+        try {
+            Connection connection = getConnection();
+            if (connection != null) {
+                PreparedStatement ps = connection.prepareStatement(query);
+                ps.setString(1, country);
+                ResultSet rs = ps.executeQuery();
+                if (rs.next()) {
+                    String code = rs.getString("Code");
+                    connection.close();
+                    return code;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static List<City> getCities(String country){
+        //prepare statement
+        List<City> cities = new ArrayList<>();
+        String query = "SELECT city.Name, JSON_EXTRACT(Info, '$.Population') AS Population " +
+                "FROM city " +
+                "INNER JOIN country ON country.Code = city.CountryCode " +
+                "WHERE country.Name LIKE ? ORDER BY Population DESC";
+        try {
+            Connection connection = getConnection();
+            if (connection != null) {
+                //make statement
+                PreparedStatement ps = connection.prepareStatement(query);
+                //insert, update, delete executeUpdate
+                //store to ResultSet
+                ps.setString(1, country);
+                ResultSet rs = ps.executeQuery();
+                if (getCountryCode(country) == null){
+                    System.out.println("\033[31mWrong country!\033[31m");
+                    return null;
+                }
+                while (rs.next()){
+                    String name = rs.getString("Name");
+                    int pop = rs.getInt("Population");
+                    City newCity = new City(name, pop);
+                    cities.add(newCity);
+                }
+                connection.close();
+            }
+        }catch (Exception e) { e.printStackTrace(); }
+        return cities;
+    }
+
+    public static boolean isCityInCountry(String city, String country){
+        String query = "SELECT Name, CountryCode from city " +
+                "WHERE CountryCode LIKE ?";
+        try {
+            Connection connection = getConnection();
+            if (connection != null){
+                String code = getCountryCode(country);
+                if (code == null) {
+                    System.out.println("\033[31mWrong country name!\033[0m");
+                    return false;
+                }
+                PreparedStatement ps = connection.prepareStatement(query);
+                ps.setString(1, code);
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()){
+                    if (city.equalsIgnoreCase(rs.getString("Name")))
+                        return true;
+                }
+                connection.close();
+            }
+        } catch (Exception e) { e.printStackTrace(); }
+        return false;
+    }
+
+    public static boolean isCityInCountryCode(String city, String code3){
+        String query = "SELECT Name, CountryCode from city " +
+                "WHERE CountryCode LIKE ?";
+        if (code3 == null) {
+            System.out.println("\033[31mWrong country name!\033[0m");
+            return false;
+        }
+        try {
+            Connection connection = getConnection();
+            if (connection != null){
+
+                PreparedStatement ps = connection.prepareStatement(query);
+                ps.setString(1, code3);
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()){
+                    if (city.equalsIgnoreCase(rs.getString("Name")))
+                        return true;
+                }
+                connection.close();
+            }
+        } catch (Exception e) { e.printStackTrace(); }
+        return false;
+    }
+
+    public static int getPreviousPop(String country, String city) {
+        int previousPop = 0;
+        for (City city1 : getCities(country)){
+            if (city1.getName().equals(city)) {
+                previousPop = city1.getPopulation();
+                break;
+            }
+        }
+        return previousPop;
+    }
+
+    public static int getMonumentId(){
+        String query = "SELECT * FROM monument";
+        int count = 0;
+        try {
+            Connection connection = getConnection();
+            if (connection != null) {
+                PreparedStatement ps = connection.prepareStatement(query);
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    count = rs.getInt("id");
+                }
+            }
+        } catch (Exception e) { e.printStackTrace(); }
+        return count;
+    }
+
+    public static Integer getCityId(String city){
+        String query = "SELECT ID, Name " +
+                "FROM city " +
+                "WHERE Name LIKE ?";
+        try {
+            Connection connection = getConnection();
+            if (connection != null){
+                PreparedStatement ps = connection.prepareStatement(query);
+                ps.setString(1, city);
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()){
+                    if (rs.getString("Name").equalsIgnoreCase(city)){
+                        return rs.getInt("ID");
+                    }
+                }
+            }
+        } catch (Exception e) { e.printStackTrace(); }
+        return null;
+    }
+
+}
